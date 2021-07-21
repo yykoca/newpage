e1e0ce72bb03f620f0cf40ede30cbfea2f22517a
yykoca <yykoca.08@gmail.com>
2021-07-21 13:40:21 +0200

    CityData info created

--- src/Components/WheatherCity.js
+++ src/Components/WheatherCity.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -8,20 +8,20 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 
-function WheatherCity({cityData}) {
-    const useStyles = makeStyles({
-        root: {
-          maxWidth: 345,
-        },
-        media: {
-          height: 140,
-        },
-      });
-    
-      const classes = useStyles();
-    return (
-        <div>
-            <Card className={classes.root}>
+function WheatherCity({ cityData }) {
+  const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+
+  const classes = useStyles();
+  return (
+    <div>
+      <Card className={classes.root}>
         <CardActionArea>
           <CardMedia
             className={classes.media}
@@ -33,14 +33,10 @@ function WheatherCity({cityData}) {
               {cityData.name}
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
-              {cityData.name}{cityData.timezone}
-              {/* City ID = {cityData.id} <br />
-              Coord = Lon: {cityData.coord.lon} Lat: {cityData.coord.lat} <br />
-              Temp = {cityData.main.temp} <br />
-              Max Temp = {cityData.main.temp_max} <br />
-              Min Temp = {cityData.main.temp_min} <br />
-              Feels Like = {cityData.main.feels_like} <br />
-              Sea Level = {cityData.main.sea_level} <br /> */}
+              ID: {cityData.id} <br />
+              Name: {cityData.name} <br />
+              Lat: {cityData.lat} <br />
+              Lon: {cityData.lon} <br />
             </Typography>
           </CardContent>
         </CardActionArea>
@@ -53,8 +49,8 @@ function WheatherCity({cityData}) {
           </Button>
         </CardActions>
       </Card>
-        </div>
-    )
+    </div>
+  );
 }
 
-export default WheatherCity
+export default WheatherCity;
