c727568d369a29ca326a943278715cfdd43ca6de
yykoca <yykoca.08@gmail.com>
2021-07-25 15:53:06 +0200

    apiKey as a parameter added

--- src/API/CWA.js
+++ src/API/CWA.js
@@ -12,8 +12,7 @@ function makeCityWheatherDetails(cityData) {
   };
 }
 
-async function getWheatherByCityID(cityID) {
-  const apiKey = "7acb6899f20cac69098920ef1aeb6365";
+async function getWheatherByCityID(cityID, apiKey) {
   const url = `https://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${apiKey}`;
 
   return await fetch(url)
