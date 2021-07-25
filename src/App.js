c7b413b039fd55fbd804b34b119df36a6fbec210
yykoca <yykoca.08@gmail.com>
2021-07-25 15:50:26 +0200

    type: module added

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import Search from "./Components/Search";
 import SearchCity from "./Components/SearchCity";
 import WheatherCity from "./Components/WheatherCity";
 import "./App.css";
+// import dotenv from 'dotenv';
+// dotenv.config()
 
 function App() {
   const [city, setCity] = useState("");
   const [cityID, setCityID] = useState(2864475);
   const [cityData, setCityData] = useState({});
+  console.log("abs", process.env);
 
   useEffect(() => {
-    CWA.getWheatherByCityID(cityID).then((dataOfCity) =>
+    CWA.getWheatherByCityID(cityID, "7acb6899f20cac69098920ef1aeb6365").then((dataOfCity) =>
+    // CWA.getWheatherByCityID(cityID, process.env.API_KEY).then((dataOfCity) =>
       setCityData(dataOfCity)
     );
   }, [cityID]);
