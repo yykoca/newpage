cf4ad6ac7f65d235fb3a9aeeb8453778e03232f9
yykoca <yykoca.08@gmail.com>
2021-07-27 14:36:09 +0200

    dotenv.config

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
+// import dotenv from "dotenv";
 import { React, useState, useEffect } from "react";
 import data from "./city.list.json";
 import CWA from "./API/CWA";
-import Search from "./Components/Search";
+// import Search from "./Components/Search";
 import SearchCity from "./Components/SearchCity";
 import WheatherCity from "./Components/WheatherCity";
 import "./App.css";
-// import dotenv from 'dotenv';
-// dotenv.config()
+// dotenv.config();
 
 function App() {
-  const [city, setCity] = useState("");
   const [cityID, setCityID] = useState(2864475);
   const [cityData, setCityData] = useState({});
-  console.log("abs", process.env);
+  const [cities, setCities] = useState([]);
+  const [city, setCity] = useState(null);
 
   useEffect(() => {
-    CWA.getWheatherByCityID(cityID, "7acb6899f20cac69098920ef1aeb6365").then((dataOfCity) =>
     // CWA.getWheatherByCityID(cityID, process.env.API_KEY).then((dataOfCity) =>
-      setCityData(dataOfCity)
+    CWA.getWheatherByCityID(cityID, "7acb6899f20cac69098920ef1aeb6365").then(
+      (dataOfCity) => setCityData(dataOfCity)
     );
   }, [cityID]);
 
   return (
     <div>
-      <Search/>
-      <WheatherCity cityData={cityData} />
       <SearchCity
         data={data}
         city={city}
         setCity={setCity}
         setCityID={setCityID}
+        cities={cities}
+        setCities={setCities}
       />
-      
+      <WheatherCity cityData={cityData} />
+
+      {/* <Search city={city} setCity={setCity} cities={cities} /> */}
     </div>
   );
 }
