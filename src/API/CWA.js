67dc70acae8e212c6b92d96e2e61fb0af0f0ddc2
yykoca <yykoca.08@gmail.com>
2021-07-27 14:34:50 +0200

    sunrise and sunset convert function

--- src/API/CWA.js
+++ src/API/CWA.js
@@ -1,24 +1,34 @@
 function makeCityWheatherDetails(cityData) {
-  console.log(cityData);
   return {
     id: cityData.id,
     name: cityData.name,
     weather: cityData.weather[0].main,
-    temp: Math.floor((cityData.main.temp - 273.15)),
-    temp_max: Math.floor((cityData.main.temp_max - 273.15)),
-    temp_min: Math.floor((cityData.main.temp_min - 273.15)),
-    lon: cityData['coord'].lon,
-    lat: cityData['coord'].lat,
+    description: cityData.weather[0].description,
+    temp: Math.floor(cityData.main.temp - 273.15),
+    temp_max: Math.floor(cityData.main.temp_max - 273.15),
+    temp_min: Math.floor(cityData.main.temp_min - 273.15),
+    lon: cityData["coord"].lon,
+    lat: cityData["coord"].lat,
+    humidity: cityData.main.humidity,
+    pressure: cityData.main.pressure,
+    sunrise: convertDate(cityData.sys.sunrise),
+    sunset: convertDate(cityData.sys.sunset),
+    windDeg: cityData.wind.deg,
+    windSpeed: cityData.wind.speed,
   };
 }
 
 async function getWheatherByCityID(cityID, apiKey) {
   const url = `https://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${apiKey}`;
-
   return await fetch(url)
     .then((response) => response.json())
     .then((data) => makeCityWheatherDetails(data));
 }
 
-const CWA = {getWheatherByCityID};
+function convertDate(currentTimestamp) {
+  let date = new Date(currentTimestamp * 1000).toLocaleTimeString();
+  return date;
+}
+
+const CWA = { getWheatherByCityID };
 export default CWA;
